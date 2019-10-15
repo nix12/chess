172878dcbd5f9d895773dda65dc3c84d5441e476
nix12 <mr.goodwin@outlook.com>
2019-10-15 11:51:24 -0400

    Add README

---
+++ README.md
@@ -0,0 +1,7 @@
+Installation:
+
+In your terminal, run these commands:
+
+git clone git@github.com:nix12/chess.git
+cd chess
+./game.rb
--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-require_relative 'gameplay/engine'
+require_relative 'lib/gameplay/engine'
 
 engine = Engine.new
 engine.start
